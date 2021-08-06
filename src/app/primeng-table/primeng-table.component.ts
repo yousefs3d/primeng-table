26cb9b970f8b13672c45e77c2ef26ac3c1c0dcb6
yousefs3d <yousefs3d@gmailc.om>
2021-08-06 17:17:55 +0300

    test vcode commit

--- src/app/primeng-table/primeng-table.component.ts
+++ src/app/primeng-table/primeng-table.component.ts
@@ -5,6 +5,7 @@ import { Component, Input, OnInit } from '@angular/core';
   templateUrl: './primeng-table.component.html',
   styleUrls: ['./primeng-table.component.scss']
 })
+//Test commit
 export class PrimengTableComponent implements OnInit {
   @Input() cols: any[];
   @Input() data = [];
